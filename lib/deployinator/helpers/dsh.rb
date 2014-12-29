8c11dd13515403f917de0ff2358e4e6f6f71d062
Jayson Paul <jaysonpaul@gmail.com>
2014-12-29 21:46:03 +0000

    Adding dsh helpers and fixing references to app_context in gem

--- lib/deployinator/helpers/dsh.rb
+++ lib/deployinator/helpers/dsh.rb
@@ -8,29 +8,29 @@ module Deployinator
       def run_dsh(groups, cmd, &block)
         groups = [groups] unless groups.is_a?(Array)
         dsh_groups = groups.map {|group| "-g #{group} "}.join("")
-        run_cmd(%Q{ssh #{Deployinator.app_context['user']}@#{Deployinator.deploy_host} dsh #{dsh_groups} -r ssh -F #{dsh_fanout} "#{cmd}"}, &block)[:stdout]
+        run_cmd(%Q{ssh #{Deployinator.default_user}@#{Deployinator.deploy_host} dsh #{dsh_groups} -r ssh -F #{dsh_fanout} "#{cmd}"}, &block)[:stdout]
       end
 
       # run dsh against a given host or array of hosts
       def run_dsh_hosts(hosts, cmd, extra_opts='', &block)
         hosts = [hosts] unless hosts.is_a?(Array)
         if extra_opts.length > 0
-          run_cmd %Q{ssh #{Deployinator.app_context['user']}@#{Deployinator.deploy_host} 'dsh -m #{hosts.join(',')} -r ssh -F #{dsh_fanout} #{extra_opts} -- "#{cmd}"'}, &block
+          run_cmd %Q{ssh #{Deployinator.default_user}@#{Deployinator.deploy_host} 'dsh -m #{hosts.join(',')} -r ssh -F #{dsh_fanout} #{extra_opts} -- "#{cmd}"'}, &block
         else
-          run_cmd %Q{ssh #{Deployinator.app_context['user']}@#{Deployinator.deploy_host} 'dsh -m #{hosts.join(',')} -r ssh -F #{dsh_fanout} -- "#{cmd}"'}, &block
+          run_cmd %Q{ssh #{Deployinator.default_user}@#{Deployinator.deploy_host} 'dsh -m #{hosts.join(',')} -r ssh -F #{dsh_fanout} -- "#{cmd}"'}, &block
         end
       end
 
       def run_dsh_extra(dsh_group, cmd, extra_opts, &block)
         # runs dsh to a single group with extra args to dsh
-        run_cmd(%Q{ssh #{Deployinator.app_context['user']}@#{Deployinator.deploy_host} dsh -g #{dsh_group} -r ssh #{extra_opts} -F #{dsh_fanout} "#{cmd}"}, &block)[:stdout]
+        run_cmd(%Q{ssh #{Deployinator.default_user}@#{Deployinator.deploy_host} dsh -g #{dsh_group} -r ssh #{extra_opts} -F #{dsh_fanout} "#{cmd}"}, &block)[:stdout]
       end
 
       def hosts_for(group)
         @hosts_for ||= {}
         @hosts_for[group] ||= begin
-          dsh_file = "/home/#{Deployinator.app_context['user']}/.dsh/group/#{group}"
-          hosts = `ssh #{Deployinator.app_context['user']}@#{Deployinator.deploy_host} cat #{dsh_file}`.chomp
+          dsh_file = "/home/#{Deployinator.default_user}/.dsh/group/#{group}"
+          hosts = `ssh #{Deployinator.default_user}@#{Deployinator.deploy_host} cat #{dsh_file}`.chomp
           if $?.nil? || $?.exitstatus != 0
             raise "DSH hosts file at #{Deployinator.deploy_host}:#{dsh_file} is likely missing!"
           end
---
+++ test/unit/helpers_dsh_test.rb
@@ -0,0 +1,41 @@
+require "rubygems"
+require 'deployinator'
+require 'deployinator/helpers'
+require 'deployinator/helpers/dsh'
+require 'test/unit'
+require 'mocha/setup'
+
+include Deployinator
+include Deployinator::Helpers
+include Deployinator::Helpers::DshHelpers
+
+#
+# Set of tests for methods in our DshHelpers Module
+#
+class DshHelperTest < Test::Unit::TestCase
+
+  def test_hosts_for_ignores_comments
+    DshHelpers.expects(:`).returns("host1\n#host2\nhost3")
+    assert_equal(["host1","host3"], DshHelpers.hosts_for("foo"))
+  end
+
+  def test_hosts_for_ignores_comments_whitespace_before
+    DshHelpers.expects(:`).returns("host1\n   #host2\nhost3")
+    assert_equal(["host1","host3"], DshHelpers.hosts_for("hoo"))
+  end
+
+  def test_hosts_for
+    DshHelpers.expects(:`).returns("host1\nhost2\nhost3")
+    assert_equal(["host1","host2","host3"], DshHelpers.hosts_for("bar"))
+  end
+
+  def test_host_for_raises_when_nil
+    DshHelpers.expects(:`).returns("")
+    # $? is the return value of the previous backtick command
+    # we are mocking it
+    $?.expects(:exitstatus).returns(1)
+    assert_raises(RuntimeError) do
+      DshHelpers.hosts_for("baz")
+    end
+  end
+end
