05801577e98bae569930478aca5c2154289aeccb
nkammah <nkammah@etsy.com>
2015-05-29 17:58:57 +0000

    adding a line break after locking stack statement

--- lib/deployinator/helpers.rb
+++ lib/deployinator/helpers.rb
@@ -411,9 +411,9 @@ module Deployinator
     end
 
     def lock_pushes(stack, who, method)
-      log_and_stream("LOCKING #{stack}")
+      log_and_stream("LOCKING #{stack}<br>")
       if lock_info = push_lock_info(stack)
-        return log_and_stream("Pushes locked by #{lock_info[:who]} - #{lock_info[:method]}")
+        return log_and_stream("Pushes locked by #{lock_info[:who]} - #{lock_info[:method]}<br>")
       end
 
       dt = Time.now.strftime("%m/%d/%Y %H:%M")
