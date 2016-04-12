e04890dfb741962ab526ba6f35c1b885d39f33d0
John Goulah <jgoulah@gmail.com>
2016-04-12 22:10:32 +0000

    more brevity

--- test/unit/git_test.rb
+++ test/unit/git_test.rb
@@ -8,25 +8,19 @@ class HelpersTest < Test::Unit::TestCase
 
   def test_git_url_https
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}}).at_least_once
     assert_equal('https://www.testmagic.com/construction/drills.git', GitHelpers.git_url(:stack, 'https', false))
   end
 
   def test_git_url_default
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}}).at_least_once
     assert_equal('git://www.testmagic.com/construction/drills', GitHelpers.git_url(:stack))
   end
 
   def test_git_url_read_write
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
-    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}}).at_least_once
     assert_equal('git@www.testmagic.com:construction/drills', GitHelpers.git_url(:stack, "git", true))
   end
 
