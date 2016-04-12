66ea6a263e8f7569d4b5eeb2d28079cf86086d42
John Goulah <jgoulah@gmail.com>
2016-04-12 18:01:50 +0000

    fix tests

--- test/unit/git_test.rb
+++ test/unit/git_test.rb
@@ -10,6 +10,7 @@ class HelpersTest < Test::Unit::TestCase
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     assert_equal('https://www.testmagic.com/construction/drills.git', GitHelpers.git_url(:stack, 'https', false))
   end
 
@@ -17,11 +18,13 @@ class HelpersTest < Test::Unit::TestCase
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     assert_equal('git://www.testmagic.com/construction/drills', GitHelpers.git_url(:stack))
   end
 
   def test_git_url_read_write
     GitHelpers.expects(:which_github_host).returns("www.testmagic.com")
+    GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     GitHelpers.expects(:git_info_for_stack).returns({:stack => {:repository => 'drills', :user => 'construction'}})
     assert_equal('git@www.testmagic.com:construction/drills', GitHelpers.git_url(:stack, "git", true))
